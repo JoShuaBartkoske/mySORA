49c7b34488480fc846acd01871e3b205083677b1
JoShuaBartkoske <shua4347@gmail.com>
2025-03-26 12:26:10 -0600

    no logging of mpc name

--- DR8_predictions.py
+++ DR8_predictions.py
@@ -63,7 +63,6 @@ occultation_found = False
 for n in MPC_bodies:
     asteroid = Body(name=f'{n}')
     print(f"MPC Body {asteroid.shortname}")
-    logging(f"MPC Body {asteroid.shortname}")
     print(f"Occultation Found Boolean is: {occultation_found}")
     if occultation_found:
         new_pred = prediction(body=asteroid, time_beg='2025-01-18',time_end='2025-02-07',mag_lim={'B': 13.5}, reference_center=T1)
